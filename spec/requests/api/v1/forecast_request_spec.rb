2ebff6f70e1866a5a6606a5d88b4acda994015c5
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-09-14 17:38:32 -0600

    test round 1

---
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe "Forecast Endpoint" do
+  before(:each) do
+    get '/api/v1/forecast?location=denver,co'
+  end
+
+  it "response is successful" do
+    expect(response).to be_successful
+  end
+
+  xit "sends forecast details" do
+  end
+end
+
+#  Current weather type (e.g. partly cloudy)
+#   Today: mostly cloudy until afternoon
+#   Tonight: partly cloudy starting tonight, continuing until tomorrow morning
+#   Current temp
+#   High & low for today
+#   Feels like
+#   Humidity %
+#   Visibility (miles)
+#   UV index: 2 (low)
+#   Forecasted temperature for each of the next 8 hours
+#   For the next 5 days, "sunny" / "partly cloudy" / etc
+#   For the next 5 days, 90% (chance of precipitation) -- & precipitation type?
+#   For the next 5 days, the forecasted high temperature
