2ff8a84c11074a4f2da13ec4a487f6288879dc5c
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-09-14 22:07:36 -0600

    Spelled unsplash wrong...

--- app/services/background_service.rb
+++ app/services/background_service.rb
@@ -4,13 +4,13 @@ class BackgroundService
   end
 
   def retrieve_background
-    upsplash_response
+    unsplash_response
   end
 
   private
     attr_reader :location
 
-    def upsplash_connection
+    def unsplash_connection
         Faraday.new(:url => 'https://api.unsplash.com') do |f|
         f.params['client_id'] = ENV['UNSPLASH_ACCESS_KEY']
         f.params['query'] = location
@@ -18,8 +18,8 @@ class BackgroundService
       end
     end
 
-    def upsplash_response
-      response = upsplash_connection.get '/photos/random'
+    def unsplash_response
+      response = unsplash_connection.get '/photos/random'
       JSON.parse(response.body, symbolize_names: true)[:urls][:full]
     end
 end
