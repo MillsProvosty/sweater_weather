7702677468144e067605ab861de57c6484da7463
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-09-15 17:28:40 -0600

    completes sessions create

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,11 +1,10 @@
 class Api::V1::SessionsController < ApplicationController
   def create
-    binding.pry
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       render status: :created, json: { api_key: user.api_key }
     else
-
+      render status: :bad_request, json: { error: user.errors.full_messages.join('. ') }
     end
   end
 end
