33fa889eb78c289a1d97a777a57f903a354ca2d2
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-09-15 15:33:31 -0600

    Adds factory bot support file

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of :email }
+    it { should validate_uniqueness_of(:email).case_insensitive }
+  end
+
+  it "#get_api_key" do
+    user1 = create(:user)
+    user2 = create(:user)
+
+    expect(user1.api_key).to_not be_nil
+    expect(user1.api_key).to_not eq(user2.api_key)
+  end
 end
