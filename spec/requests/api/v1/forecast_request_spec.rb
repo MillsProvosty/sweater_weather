d8d6c13f69c56789c1796f90c8cb9cf09fe1f0fd
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-09-14 20:08:17 -0600

    Final test for forecast

--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -19,10 +19,13 @@ describe "Forecast Endpoint" do
 
     forecast = JSON.parse(response.body)
 
-    binding.pry
     expect(forecast["currently"]["summary"]).to be_kind_of(String)
     expect(forecast["latitude"]).to be_kind_of(Float)
+    expect(forecast["longitude"]).to be_kind_of(Float)
     expect(forecast["currently"]["time"]).to be_kind_of(Integer)
+    expect(forecast["currently"]["summary"]).to be_kind_of(String)
+    expect(forecast["currently"]["humidity"]).to be_kind_of(Float)
+    expect(forecast["currently"]["uvIndex"]).to be_kind_of(Integer)
   end
 end
 
