8aa3dca0f4be0ba5cfab9113c2743557b891001e
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-09-15 17:24:44 -0600

    Adds sessions controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,5 +1,11 @@
 class Api::V1::SessionsController < ApplicationController
   def create
     binding.pry
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      render status: :created, json: { api_key: user.api_key }
+    else
+
+    end
   end
 end
