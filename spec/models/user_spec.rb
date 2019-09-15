e423d464825cd918e439040e6d8142c2dd14fd22
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-09-15 15:40:14 -0600

    Adds model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,11 +6,9 @@ RSpec.describe User, type: :model do
     it { should validate_uniqueness_of(:email).case_insensitive }
   end
 
-  it "#get_api_key" do
+  it "#api_key" do
     user1 = create(:user)
-    user2 = create(:user)
-
-    expect(user1.api_key).to_not be_nil
-    expect(user1.api_key).to_not eq(user2.api_key)
+    u1key = user1.api_key
+    expect(u1key).to_not be_nil
   end
 end
