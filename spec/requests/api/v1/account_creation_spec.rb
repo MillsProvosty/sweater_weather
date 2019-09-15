b7a1f73c2396d371b09951e4b64030a07511b915
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-09-15 14:23:44 -0600

    Adds account spec

---
+++ spec/requests/api/v1/account_creation_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe 'Account Creation Endpoint' do
+  before :each do
+    @params = {
+      "email": "whatever@example.com",
+      "password": "password"
+      "password_confirmation": "password"
+    }
+
+    get '/api/v1/users', params: @params
+
+    @user = User.last
+  end
+
+  it 'Returns api key upon account creation' do
+
+    expect(reponse).to be_successful
+
+    api_key = response.body
+    binding.pry
+  end
+end
