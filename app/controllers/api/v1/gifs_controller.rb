27956a935131d45133e48adc4f79567c512566dd
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-09-16 12:00:50 -0600

    Refactors controller

--- app/controllers/api/v1/gifs_controller.rb
+++ app/controllers/api/v1/gifs_controller.rb
@@ -1,18 +1,5 @@
 class Api::V1::GifsController < ApplicationController
   def index
-    GatherService.new(params[:location]).return_all_the_stuff
-    # forecast_summary = ForecastFacade.new(params[:location]).return_forecast
-    #
-    # summaries = []
-    # forecast_summary[:daily][:data][0..4].each do |f|
-    #   summaries << Summary.new(f[:summary], f[:time])
-    # end
-    #
-    # gif_urls = []
-    # summaries.map do |s|
-    #   gif_urls << GifsFacade.new(s.summary).return_gifs
-    # end
-
-    render json: GiphySearch.new(summaries, gif_urls).renders_hash
+    render json: GatherService.new(params[:location]).return_all
   end
 end
