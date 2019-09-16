adfa1cb95e13d1c79bd840bac60e35fdf352b872
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-09-15 20:46:13 -0600

    Adds error message to sessions controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,12 +5,16 @@ class Api::V1::UsersController < ApplicationController
     if user.save
       render status: :created, json: {api_key: user.api_key}
     else
-      render status: :bad_request, json: { error: user.errors.full_messages.join('. ') }
+      error
     end
   end
 
   private
     def user_params
       params.permit(:email, :password, :password_confirmation)
+    end
+
+    def error
+      render status: :bad_request, json: { error: "There was an error, please try again." }
     end
 end
