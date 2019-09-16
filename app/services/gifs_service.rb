70f3f56e40decb834fbb7c97ba5d0dfe46edb46f
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-09-16 11:43:40 -0600

    Refactors service

--- app/services/gifs_service.rb
+++ app/services/gifs_service.rb
@@ -4,17 +4,22 @@ class GifsService
   end
 
   def search_gifs
-
-  conn = Faraday.new(:url => 'http://api.giphy.com') do |f|
-    f.params['api_key'] = ENV['GIPHY_API_KEY']
-    f.params['q'] = summary
-    f.adapter  Faraday.default_adapter
-  end
-
-  response = conn.get '/v1/gifs/search'
-  url = JSON.parse(response.body, symbolize_names: true)[:data].first[:images][:original_still][:url]
+    giphy_response
   end
 
   private
     attr_reader :summary
+
+    def giphy_connection
+      Faraday.new(:url => 'http://api.giphy.com') do |f|
+        f.params['api_key'] = ENV['GIPHY_API_KEY']
+        f.params['q'] = summary
+        f.adapter  Faraday.default_adapter
+      end
+    end
+
+    def giphy_response
+      response = giphy_connection.get '/v1/gifs/search'
+      JSON.parse(response.body, symbolize_names: true)[:data].first[:images][:original_still][:url]
+    end
 end
