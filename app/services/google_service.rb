7024d4ab21441baedf8d02863e9032c07050b76f
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-09-16 16:11:43 -0600

    Google now returns json object with lat and long

---
+++ app/services/google_service.rb
@@ -0,0 +1,26 @@
+class GoogleService
+  def initialize(location)
+    @location = location
+  end
+
+  def geocode_location
+    JSON.parse(geo_response.body, symbolize_names: true)
+  end
+
+  private
+
+    attr_reader :location
+
+    def geocode_connection
+      Faraday.new('https://maps.googleapis.com') do |f|
+        f.params['address'] = location
+        f.params['key'] = ENV['GOOGLE_API_KEY']
+        f.adapter Faraday.default_adapter
+      end
+    end
+
+    def geo_response
+      response = geocode_connection.get('maps/api/geocode/json?')
+      JSON.parse(response.body, symbolize_names: true)
+    end
+end
