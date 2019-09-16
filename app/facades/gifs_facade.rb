ecd0f9b8e433a93847e9d04962c74bb4c4a00529
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-09-16 09:27:37 -0600

    connection to giphy working

--- app/facades/gifs_facade.rb
+++ app/facades/gifs_facade.rb
@@ -4,7 +4,16 @@ class GifsFacade
   end
 
   def return_gifs
-    
+    conn = Faraday.new(:url => 'http://api.giphy.com') do |f|
+      f.params['api_key'] = ENV['GIPHY_API_KEY']
+      f.params['q'] = query
+      f.params['limit'] = 5
+      f.adapter  Faraday.default_adapter
+    end
+
+    response = conn.get '/v1/gifs/search'
+    this = JSON.parse(response.body)
+    binding.pry
   end
 
   private
