ae652f646c69fac7cb1943f63fd2c1d8251cc164
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-09-17 14:58:14 -0600

    Holy cow...refactored google service...and it WORKS

--- app/facades/road_trips_facade.rb
+++ app/facades/road_trips_facade.rb
@@ -5,26 +5,12 @@ class RoadTripsFacade
   end
 
   def road_trip_response
-    this = google_response
-    this["routes"].first["legs"].first["distance"]["value"]
+    time = GoogleService.new.travel_time(@origin, @destination)
+    time[:routes].first[:legs].first[:distance][:value]
   end
 
   private
 
-    def google_connection
-      conn = Faraday.new(:url => 'https://maps.googleapis.com') do |f|
-        f.params["origin"] = origin
-        f.params["destination"] = destination
-        f.params["key"] = ENV["GOOGLE_API_KEY"]
-        f.adapter  Faraday.default_adapter
-      end
-    end
-
-    def google_response
-      response = google_connection.get('maps/api/directions/json?')
-      JSON.parse(response.body, symbolize_names: true)
-    end
-    
     attr_reader :origin,
                 :destination
 end
--- app/services/google_service.rb
+++ app/services/google_service.rb
@@ -1,26 +1,27 @@
 class GoogleService
-  def initialize(location)
-    @location = location
+  
+  def geocode_location(location)
+    geo_response('maps/api/geocode/json?', { address: location} )
   end
 
-  def geocode_location
-    geo_response
+  def travel_time(origin, destination)
+    geo_response('maps/api/directions/json?', { origin: origin, destination: destination} )
   end
-  
+
+
   private
 
     attr_reader :location
 
     def geocode_connection
       Faraday.new('https://maps.googleapis.com') do |f|
-        f.params['address'] = location
         f.params['key'] = ENV['GOOGLE_API_KEY']
         f.adapter Faraday.default_adapter
       end
     end
 
-    def geo_response
-      response = geocode_connection.get('maps/api/geocode/json?')
+    def geo_response(url, params)
+      response = geocode_connection.get(url, params)
       JSON.parse(response.body, symbolize_names: true)
     end
 end
