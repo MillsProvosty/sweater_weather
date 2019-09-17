7974793d21cc771d40582fea52e2713fa935567d
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-09-16 20:00:08 -0600

    Gameplan set

--- app/controllers/api/v1/road_trips_controller.rb
+++ app/controllers/api/v1/road_trips_controller.rb
@@ -5,11 +5,18 @@ class Api::V1::RoadTripsController < ApplicationController
       destination = params["destination"]
       user = User.find_by(api_key: api_key)
     if user
-      render json: RoadTripSerializer.new(RoadTripsFacade.new(origin, destination).response)
+      render json: RoadTripsFacade.new(origin, destination).response
     else
       trip_error
     end
 
+  # I want code that will return the travel time from two places - Make an object of this
+  # then I need code that will take the travel time and enter it into the darksky connection
+  # Then format the response to have temperature, Summary and that original estimated travel time.
+  #Step 1: Facade => calls google API Service, gets hash with info, create/return distance PORO
+  #step 2: Facade => calls Darksky API Service, gets Weather, Creates Roadtrip object that passes/returns
+  #distance and All the info, FORMAT the JSON Response in here.
+  #Step 3: Facade calls Serializer= sets attributes
 
   end
 
