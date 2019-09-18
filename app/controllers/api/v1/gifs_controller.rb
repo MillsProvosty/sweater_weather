0e893e48d9ff2939bd9fb474212e8476286de027
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-09-18 07:40:41 -0600

    Adds skip before action

--- app/controllers/api/v1/gifs_controller.rb
+++ app/controllers/api/v1/gifs_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::GifsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: :create
+  
   def index
     render json: GifForecastFacade.new(params[:location]).return_all
   end
