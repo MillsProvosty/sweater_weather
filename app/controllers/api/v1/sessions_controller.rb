58c0369776613b63990f94097498894fe9aedcba
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-09-17 19:30:56 -0600

    Changes controllers to not authrize users

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,8 +1,10 @@
 class Api::V1::SessionsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: :create
+  
   def create
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
-      render status: :created, json: { api_key: user.api_key }
+      render json: { api_key: user.api_key }, status: :created
     else
       authentication_error
     end
